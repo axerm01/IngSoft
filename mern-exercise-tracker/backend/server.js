bf12b1feb2a9b092f6983e4576993d82aeb2d8ef
Alex <140414931+axerm01@users.noreply.github.com>
2024-07-01 23:50:33 +0200

    Add files via upload

---
+++ mern-exercise-tracker/backend/server.js
@@ -0,0 +1,39 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const cors = require('cors');
+const dotenv = require('dotenv');
+
+dotenv.config();
+
+const app = express();
+const port = process.env.PORT || 5000;
+
+// Middleware
+app.use(cors());
+app.use(express.json());
+
+// Connessione a MongoDB
+mongoose.connect("mongodb://localhost:27017/Prova", {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+}).then(() => {
+    console.log('Connected to MongoDB');
+}).catch((error) => {
+    console.error('MongoDB connection error:', error);
+});
+
+// Route di esempio
+app.get('/', (req, res) => {
+    res.send('NABROOO?? World');
+});
+
+const exercisesRouter = require('./routes/exercises');
+const usersRouter = require('./routes/users');
+
+app.use('/exercises', exercisesRouter);
+app.use('/users', usersRouter);
+
+// Avvio del server
+app.listen(port, () => {
+    console.log(`Server running at http://localhost:${port}/`);
+});

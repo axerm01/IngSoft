96ae0854058958de20226dc188c20f5e328f4f43
AleDeso <129870523+AleDeso@users.noreply.github.com>
2024-07-16 00:09:21 +0200

    Upload pages - Gioca

--- ing_soft_frontend/src/pages/Home.js
+++ ing_soft_frontend/src/pages/Home.js
@@ -1,28 +1,35 @@
-// src/Home.js
-import React, { useState } from 'react';
-import { Link,useNavigate } from 'react-router-dom';
-import './Home.css';
-
+// src/pages/Home.js
+import React from 'react';
+import { Link, useNavigate, useLocation } from 'react-router-dom';
+import { useAuth } from '../context/AuthContext';
+import '../style/Home.css';
 
 const Home = () => {
-    const navigate = useNavigate();
-    const [authenticated, setAuthenticated] = useState(true); // Stato di autenticazione
-    const handleLogout = () => {
-    setAuthenticated(false);
-    navigate('/LogIn');
+  const navigate = useNavigate();
+  const { logout } = useAuth();
+  const location = useLocation();
+  const params = new URLSearchParams(location.search);
+  const username = params.get('username');
+
+  const handleLogout = () => {
+    logout();
+    navigate('/login');
   };
-    return (
-      <div className="home-container">
-        <header className="home-header">
-          <h1 className="title">Benvenuto in AGAFabula</h1>
-          <button className="logout-button" onClick={handleLogout} >Logout</button>
-        </header>
-        <div className="button-container">
-          <Link to="/Scene" className="home-button">Le tue scene</Link>
-          <Link to="/stories" className="home-button">Crea storia</Link>
-        </div>
+
+  return (
+    <div className="home-container">
+      <header className="home-header">
+        <h1 className="titleHome">Benvenuto in AGAFabula, {username}!</h1>
+        <button className="logout-button" onClick={handleLogout}>Logout</button>
+      </header>
+      <div className="button-container">
+        <Link to={`/item?username=${encodeURIComponent(username)}`} className="home-button">Oggetti</Link>
+        <Link to={`/createStory?username=${encodeURIComponent(username)}`} className="home-button">Configurazione</Link>
+        <Link to={`/createScene?username=${encodeURIComponent(username)}`} className="home-button">Scene</Link>
+        <Link to={`/createStory?username=${encodeURIComponent(username)}`} className="home-button">Crea Storia</Link>
       </div>
-    );
-  };
+    </div>
+  );
+};
 
 export default Home;

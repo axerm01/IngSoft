184e363d92d2dbaa18e4c355ade7a0a07ab65dac
AleDeso <129870523+AleDeso@users.noreply.github.com>
2024-07-16 00:07:19 +0200

    Upload route - Gioca

--- ing_soft_backend/src/routes/scenes.route.js
+++ ing_soft_backend/src/routes/scenes.route.js
@@ -1,21 +1,57 @@
+// routes/scenes.js
+
 const router = require('express').Router();
-const Scene = require('../models/scene.model'); // Assicurati che il modello User sia correttamente importato
+const Scene = require('../models/scene.model');
 
 // Aggiungi una nuova scena
 router.route('/add').post(async (req, res) => {
-    const { title, description, sceneType } = req.body;
-
+    const { authorName, storyTitle, title, description, sceneType, requiredObject, foundObject } = req.body;
     try {
-        // Controlla se la scena esiste già
-        const existingScene = await Scene.findOne({ title });
-        if (existingScene) {
-            return res.status(400).json('Scene already exists');
-        }
+        const newScene = new Scene({
+            authorName,
+            storyTitle,
+            title,
+            description,
+            sceneType,
+            requiredObject,
+            foundObject,
+        });
 
-        // Crea una nuova scena
-        const newScene = new Scene({ title, description, sceneType });
         await newScene.save();
         res.json('Scene added!');
+    } catch (error) {
+        res.status(400).json('Error: ' + error);
+    }
+});
+
+router.route('/story/:storyTitle/author/:authorName').get(async (req, res) => {
+    const { storyTitle, authorName } = req.params;
+
+    try {
+        const scenes = await Scene.find({ storyTitle, authorName });
+        res.json(scenes);
+    } catch (error) {
+        res.status(400).json('Error: ' + error);
+    }
+});
+
+// Modifica la descrizione di una scena basata sul titolo della storia e della scena
+router.route('/edit/:storyTitle/:sceneTitle').put(async (req, res) => {
+    const { storyTitle, sceneTitle } = req.params;
+    const { description } = req.body;
+
+    try {
+        const scene = await Scene.findOneAndUpdate(
+            { storyTitle: storyTitle, title: sceneTitle },
+            { description: description },
+            { new: true }
+        );
+
+        if (!scene) {
+            return res.status(404).json('Scene not found');
+        }
+
+        res.json('Scene description updated');
     } catch (error) {
         res.status(400).json('Error: ' + error);
     }
@@ -31,41 +67,30 @@ router.route('/').get(async (req, res) => {
     }
 });
 
-// Ottieni una scena dato il suo titolo
-router.route('/:title').get(async (req, res) => {
-    const { title } = req.params;
+// Ottieni tutte le scene di un autore
+router.route('/author/:authorName').get(async (req, res) => {
+    const { authorName } = req.params;
 
     try {
-        const scene = await Scene.findOne({ title });
-        if (!scene) {
-            return res.status(404).json('Scena not found');
-        }
-        res.json(scene);
+        const scenes = await Scene.find({ authorName });
+        res.json(scenes);
     } catch (error) {
         res.status(400).json('Error: ' + error);
     }
 });
 
-// Elimina tutte le scene
-router.route('/delete/all').delete(async (req, res) => {
-    try {
-        await Scene.deleteMany();
-        res.json('All scenes deleted');
-    } catch (error) {
-        res.status(400).json('Error: ' + error);
-    }
-});
-
-// Elimina una scena dato il titolo
-router.route('/delete/:title').delete(async (req, res) => {
-    const { title } = req.params;
+// Verifica se una scena esiste già basata sul titolo della storia, il titolo della scena e il nome dell'autore
+router.route('/check').get(async (req, res) => {
+    const { storyTitle, title, authorName } = req.query;
 
     try {
-        const deletedScene = await Scene.findOneAndDelete({ title });
-        if (!deletedScene) {
-            return res.status(404).json('Scene not found');
+        const scene = await Scene.findOne({ storyTitle, title, authorName });
+
+        if (scene) {
+            return res.json({ exists: true });
+        } else {
+            return res.json({ exists: false });
         }
-        res.json('Scene deleted');
     } catch (error) {
         res.status(400).json('Error: ' + error);
     }

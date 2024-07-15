adce6f52dbeea79cb437cb0137766b2ea6939104
AleDeso <129870523+AleDeso@users.noreply.github.com>
2024-07-16 00:06:31 +0200

    upload models - Gioca

--- ing_soft_backend/src/models/scene.model.js
+++ ing_soft_backend/src/models/scene.model.js
@@ -2,13 +2,17 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const sceneSchema = new Schema({
+    authorName: { type: String, required: true },
+    storyTitle: { type: String, required: true },
     title: { type: String, required: true },
     description: { type: String, required: true },
-    sceneType: { 
-        type: String, 
+    sceneType: {
+        type: String,
         required: true,
         enum: ['scelta', 'indovinello', 'finale']
-    }
+    },
+    requiredObject: { type: String },
+    foundObject: { type: String }
 });
 
 const Scene = mongoose.model('Scene', sceneSchema);
